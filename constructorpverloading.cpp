6a3cd0a664de9bdca8cc177fb4b61c8cea56d896
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-05-05 18:07:38 +0700

    definisi nim dan nama

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -24,3 +24,13 @@ mahasiswa :: mahasiswa(int iNim){
 mahasiswa :: mahasiswa(string iNama){
     nama = iNama; 
 }
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; 
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama= " << nama << endl;
+}
