30e6a899cd7a8279ab7530e9e08c38d84434ee8e
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-05-05 17:22:53 +0700

    pembuatan static function

--- staticfuncution.cpp
+++ staticfuncution.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+class Mahasiswa {
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static void getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
+};
