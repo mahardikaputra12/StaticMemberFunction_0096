1b521dafc6b4b7740cfb0eadd5425f9a3dce8d3b
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-05-05 18:06:44 +0700

    definisi hanya nim dan nama

---
+++ constructorpverloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+    private:
+        int nim;
+        string nama;
+    public:
+        mahasiswa();
+        mahasiswa(int);
+        mahasiswa(string);
+        mahasiswa(int iNim, string iNama);
+        void cetak();
+};
+
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; 
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; 
+}
