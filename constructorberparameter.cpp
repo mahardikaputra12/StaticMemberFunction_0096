b3c438f986ba0236610c9257c06277b2374735ab
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-05-05 17:58:25 +0700

    memanggil contructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,15 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama);
 };
+
+
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" <<endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main (){
+    mahasiswa mhs(113, "Rizki Tukang Makan");
+    return 0;
+}
